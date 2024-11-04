ecdd3241211d506d7c03fd4ee4b6f9ac5b1b9b9c
Batu Kuscuoglu <kuscuoglu.batu@gmail.com>
2024-11-04 19:51:38 +0300

    images-added

--- src/App.jsx
+++ src/App.jsx
@@ -1,11 +1,17 @@
 import React from 'react';
+import { BrowserRouter as Router, Route, Routes } from 'react-router-dom';
 
+import Home from './components/Home';
+import SignIn from './components/Login';
+import About from './components/About';
 
 function App() {
   return (
     <Router>
       <Routes>
-
+        <Route path="/" element={<Home />} />
+        <Route path="/login" element={<SignIn />} />
+        <Route path="/about" element={<About />} />
       </Routes>
     </Router>
   );
